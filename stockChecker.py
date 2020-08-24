5af9c1b627f55282f85c123b8240478173fe6269
Jeremy Silver <me@jeremysilver.com>
2020-08-24 16:39:42 -0700

    Regenerate configuration files Add requirements.txt Clean up code syntax for current style guidelines

--- stockChecker.py
+++ stockChecker.py
@@ -14,7 +14,7 @@ import logging
 import os
 import re
 import requests
-import sys
+# import sys
 from datetime import datetime
 from twilio.rest import Client
 
@@ -25,15 +25,15 @@ global_config = os.getcwd() + '/' + 'config.json'
 debug_datafile = os.getcwd() + '/' + 'debugdata.dat'
 
 
-def load_user_config(user_config_file='~/stocklist.json'):
+def load_user_config(user_config_file='stocklist.json'):
     user_config_file = os.path.expanduser(user_config_file)
     if os.path.isfile(user_config_file):
-        logging.debug('Reading user_config_file from %s' % user_config_file)
+        # logging.debug('Reading user_config_file from %s' % user_config_file)
         with open(user_config_file) as fs:
             json_config = json.load(fs)
         return json_config
     else:
-        logging.error("Missing user_config_file file %s" % user_config_file)
+        # logging.error("Missing user_config_file file %s" % user_config_file)
         return None
 
 
@@ -42,31 +42,32 @@ def get_app_config(key):
         with open(global_config) as fs:
             return json.load(fs)[key]
     except Exception as exc:
-        logging.critical('Failed to open {}: {}'.format(global_config, exc))
+        logging.exception('Failed to open {}: {}'.format(global_config, exc))
+        return None
 
 
 def get_stock_price(series, date):
-    logging.debug('Getting price info for {} from {}'.format(date, series))
+    # logging.debug('Getting price info for {} from {}'.format(date, series))
     price_update = {}
 
     series_dates = sorted(series.keys())
     previous_update = series_dates[series_dates.index(date) - 1]
-    logging.debug('Previous date: {} @ {}'.format(previous_update, series[previous_update]['4. close']))
+    # logging.debug('Previous date: {} @ {}'.format(previous_update, series[previous_update]['4. close']))
 
     price_update['price'] = float(series[date]['4. close'])
     price_update['change'] = (price_update['price'] - float(series[previous_update]['4. close']))
     price_update['change_pct'] = (price_update['change'] / price_update['price']) * 100
-    logging.debug('Price: {}\tChange: {}\tPct:{}'.format(price_update['price'], price_update['change'],
-                                                         price_update['change_pct']))
+    # logging.debug('Price: {}\tChange: {}\tPct:{}'.format(price_update['price'], price_update['change'],
+    #                                                      price_update['change_pct']))
     return price_update
 
 
-def get_stock_updates(tickers='CSCO'):
+def get_stock_updates(tickers='SPY'):
     av_config = get_app_config('alphavantage')
     results = {}
     ticker_update = {}
 
-    logging.debug('Tickers to process: %s' % tickers)
+    # logging.debug('Tickers to process: %s' % tickers)
 
     for ticker in tickers:
         json_result = None
@@ -90,13 +91,13 @@ def get_stock_updates(tickers='CSCO'):
                 json_result = json.loads(res.text)
             else:
                 # Test data (end-of-day)
-                logging.debug('Loading debug data: {}'.format(debug_datafile))
+                # logging.debug('Loading debug data: {}'.format(debug_datafile))
                 with open(debug_datafile) as fs:
                     json_result = json.load(fs)
             if json_result:
-                logging.debug('Successfully retrieved data.')
+                # logging.debug('Successfully retrieved data.')
                 break
-            logging.debug('Request: {} failed, try {}'.format(av_config['api_url'], attempt + 1))
+            # logging.debug('Request: {} failed, try {}'.format(av_config['api_url'], attempt + 1))
             attempt += 1
 
         ticker_update['metadata'] = json_result['Meta Data']
@@ -108,12 +109,12 @@ def get_stock_updates(tickers='CSCO'):
         update_date = update_dt.strftime('%Y-%m-%d')
 
         if update_date != datetime.strftime(datetime.today(), '%Y-%m-%d'):
-            logging.debug('Retrieved data is not from today: {}'.format(update_date))
+            # logging.debug('Retrieved data is not from today: {}'.format(update_date))
             return None
 
         ticker_update['last'] = get_stock_price(json_result['Time Series (Daily)'], update_date)
         results[ticker] = ticker_update
-        logging.debug('Update: %s = %s' % (ticker, results[ticker]))
+        # logging.debug('Update: %s = %s' % (ticker, results[ticker]))
 
     return results
 
@@ -132,10 +133,10 @@ def send_notification(user_address, tickers):
         change_pct = tickers[ticker]['last']['change_pct']
         msg_body += ('{}  ${:.2f},  {:+.2f} ({:+.2f}%)\n'.format(ticker, close_price, close_change, change_pct))
 
-    logging.debug('New message: %s' % msg_body)
+    # logging.debug('New message: %s' % msg_body)
 
     twilio_config = get_app_config('twilio')
-    logging.debug('Twilio config: {}'.format(twilio_config))
+    # logging.debug('Twilio config: {}'.format(twilio_config))
 
     if twilio_config and all([twilio_config['phoneNo'], twilio_config['auth'], twilio_config['sid']]):
         t_client = Client(twilio_config['sid'], twilio_config['auth'])
@@ -146,29 +147,27 @@ def send_notification(user_address, tickers):
             except Exception as exc:
                 logging.error('Message send failed: {}'.format(exc))
         else:
-            logging.debug('Twilio Client generated: {}\tTest run, no notification generated.'.format(t_client))
+            # logging.debug('Twilio Client generated: {}\tTest run, no notification generated.'.format(t_client))
+            pass
     else:
-        logging.error("Missing Twilio config in {}".format(global_config))
+        # logging.error("Missing Twilio config in {}".format(global_config))
         return False
 
 
 def main():
     user_config = load_user_config()
-    logging.debug('Config: %s' % user_config)
+    # logging.debug('Config: %s' % user_config)
 
     if user_config:
         stock_update = get_stock_updates(user_config['tickers'])
         if stock_update:
             send_notification(user_config['user'], stock_update)
         else:
-            logging.debug('No stock update available.')
+            # logging.debug('No stock update available.')
+            pass
     else:
         pass
 
-
-# Check for CLI parameter 'DEBUG'
-if len(sys.argv) > 1:
-    DEBUG = True
 
 if __name__ == "__main__":
     main()
